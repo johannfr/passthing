7716e6ad83ddd7f8805b94cb208d7f0fdbc3e033
Johann Fridriksson <johann.fridriksson@gmail.com>
2016-06-09 23:06:35 +0200

    Cleaned up and fixed. And README.

---
+++ README.md
@@ -0,0 +1,21 @@
+PassThing
+=========
+
+Introduction
+------------
+
+This is a command-line password manager. That you could store on your server and get to wherever you are. Because reasons.
+
+Status
+------
+
+This was hacked together over a period of a few hours.
+
+### Commands
+
+ * new - Create a new entry.
+ * remove - Remove the specified entry
+ * modify - Modify the specified entry
+ * list - List the names of all entries
+ * exit - ...
+
--- passthing.py
+++ passthing.py
@@ -8,6 +8,8 @@ import argparse
 import pickle
 import base64
 import os
+import time
+
 from colored import fg, bg, attr
 
 from cryptography.fernet import Fernet
@@ -15,6 +17,12 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.fernet import InvalidToken
+
+class Database(object):
+    def __init__(self, master_password="", master_salt="", passwords={}):
+        self.master_password = master_password
+        self.master_salt = master_salt
+        self.passwords = passwords
 
 database = None
 database_filename = "passthing.pt"
@@ -46,7 +54,7 @@ def decrypt(master_password, text, salt):
 
 def verify_master_password(master_password):
     try:
-        if master_password != decrypt(master_password, database["MasterPassword"]["password"], database["MasterPassword"]["salt"]):
+        if master_password != decrypt(master_password, database.master_password, database.master_salt):
             return False
     except InvalidToken:
             return False
@@ -54,10 +62,10 @@ def verify_master_password(master_password):
 
 
 def save_database():
-    pickle.dump(database, open(database_filename, "wb"))
+    pickle.dump(database, open(database_filename, "wb"), protocol=2)
 
 def modifyCommand(entry_name, from_new=False):
-    if not from_new and not database.has_key(entry_name):
+    if not from_new and not database.passwords.has_key(entry_name):
         print "Entry not found: %s"%entry_name
         return
     readline.parse_and_bind('set disable-completion on')
@@ -68,18 +76,17 @@ def modifyCommand(entry_name, from_new=False):
     if not verify_master_password(master_password):
         print "Invalid master-password."
         return
-    database[entry_name] = {
+    database.passwords[entry_name] = {
         "username" : username,
         "password" : encrypt(master_password, password, salt),
         "salt" : salt
     }
     save_database()
-
     readline.parse_and_bind('set disable-completion off')
 
 def removeCommand(entry_name):
-    if database.has_key(entry_name):
-        database.pop(entry_name)
+    if database.passwords.has_key(entry_name):
+        database.passwords.pop(entry_name)
         save_database()
 
 def newCommand(entry_name):
@@ -92,6 +99,10 @@ def newCommand(entry_name):
         break
     modifyCommand(entry_name, True)
 
+def listCommand(entry_name):
+    for i in database.passwords.keys():
+        print i
+
 def exitCommand(entry_key):
     sys.exit(0)
 
@@ -99,16 +110,17 @@ commands = {
     "new" : newCommand,
     "remove" : removeCommand,
     "modify" : modifyCommand,
+    "list" : listCommand,
     "exit" : exitCommand
 }
 
 def completer(text, state):
     part = text.split()[-1]
     if len(text.split()) > 1:
-        options = [i for i in [j for j in database.keys() if j != "MasterPassword"] if part in i]
+        options = [i for i in database.passwords if part in i]
     else:
         options = [i for i in commands.keys() if i.startswith(part)]
-        options += [i for i in [j for j in database.keys() if j != "MasterPassword"] if part in i]
+        options += [i for i in database.passwords if part in i]
     if state < len(options):
         return options[state]
     else:
@@ -132,6 +144,15 @@ if __name__ == "__main__":
             default=database_filename
             )
 
+    parser.add_argument(
+            "-t",
+            "--timeout",
+            help="How long password should remain on screen",
+            nargs="?",
+            type=int,
+            default=5
+            )
+
     args = parser.parse_args(sys.argv[1:])
     database_filename = args.database
 
@@ -153,11 +174,9 @@ if __name__ == "__main__":
         master_password = getpass.getpass("Master-password: ")
         salt = os.urandom(16)
 
-        database = {}
-        database["MasterPassword"] = {
-            "password" : encrypt(master_password, master_password, salt),
-            "salt" : salt
-        }
+        database = Database()
+        database.master_password = encrypt(master_password, master_password, salt)
+        database.master_salt = salt
         save_database()
 
     readline.parse_and_bind("tab: complete")
@@ -172,13 +191,23 @@ if __name__ == "__main__":
         if line == "exit":
             break
 
-        # The check for "MasterPassword" is just so we don't get a KeyError because it has no username.
-        if len(line.split()) == 1 and line in database.keys() and line != "MasterPassword":
+        if len(line.split()) == 1 and line in database.passwords.keys():
             master_password = getpass.getpass("Master-password: ")
-            password = decrypt(master_password, database[line]["password"], database[line]["salt"])
-            print "Username: %s"%database[line]["username"]
-            print "Password: %s%s%s%s"%(fg(0), bg(0), password, attr(0))
+            password = decrypt(master_password, database.passwords[line]["password"], database.passwords[line]["salt"])
+            print "Username: %s"%database.passwords[line]["username"]
+            max_length = 0
+            for i in reversed(range(args.timeout)):
+                out_string = "\rPassword(%d): %s%s%s%s"%(i, fg(0), bg(0), password, attr(0))
+                max_length = max([max_length, len(out_string)])
+                sys.stdout.write(out_string)
+                sys.stdout.flush()
+                time.sleep(1)
+            sys.stdout.write("\r%s%s"%(" "*max_length, "\n"))
+            sys.stdout.flush()
+            
         else:
             command = line.split()[0]
             if command in commands.keys():
                 commands[command](" ".join(line.split()[1:]))
+            else:
+                print "No such command: %s"%command
